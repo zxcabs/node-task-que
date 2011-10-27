14e5062ac088d11e85779a67b64a00e376d3839e
Evgeny Reznichenko <kusakyky@gmail.com>
2011-10-27 15:29:57 +0400

    add load task

--- example/worker.js
+++ example/worker.js
@@ -1,5 +1,11 @@
+var util = require('util');
+
 var Worker = require('../').Worker,
-	email = new Worker('email', 2, function (err, tasks, done) {
-		console.log('Data: %s, Error: %s, tasks: %s', new Date(),  err, tasks);
+	email = new Worker('email', function (err, task, done) {
+		console.log('Email data: %s, Error: %s, task: %s', new Date(),  err, util.inspect(task));
 		done();
-	});
+	}),
+	pdf = new Worker('pdf', function (err, task, done) {
+		console.log('PDF data: %s, Error: %s, task: %s', new Date(),  err, task.data.address);
+		done();
+	});;
