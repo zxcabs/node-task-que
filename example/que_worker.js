03eab523c5699428532bd046752e716bcaaa6e5f
Evgeny Reznichenko <kusakyky@gmail.com>
2012-07-27 18:10:28 +0400

    должно работать!

---
+++ example/que_worker.js
@@ -0,0 +1,43 @@
+var Que = require('../').Que,
+	Worker = require('../').Worker;
+
+var que = new Que();
+
+
+setTimeout(function () {
+	console.log('push to que');
+	
+	que.task('email', { address: 'da2@da.ru' });
+	que.task('email', { address: 'da3@da.ru' });
+	que.task('email', { address: 'da4@da.ru' });
+	que.task('email', { address: 'da5@da.ru' });
+	que.task('email', { address: 'da6@da.ru' });
+	que.task('email', { address: 'da7@da.ru' });
+	que.task('email', { address: 'da8@da.ru' });
+	que.task('email', { address: 'da9@da.ru' });
+	que.task('pdf', { address: 'pdf1' });
+	que.task('pdf', { address: 'pdf2' });
+	que.task('pdf', { address: 'pdf3' });
+	que.task('pdf', { address: 'pdf4' });
+	que.task('pdf', { address: 'pdf5' });
+	que.task('pdf', { address: 'pdf6' });
+	que.task('pdf', { address: 'pdf7' });
+	que.task('email', { address: 'da@da.ru' }, function (err, task) {
+		console.log('Add task: %s, error: %s', task, err);
+		que.end();
+	});
+}, 3000);
+
+
+function emailHandler(task, done) {
+	console.log('Email: ' + task.data.address);
+	done();
+}
+
+function pdfHandler(tasks, done) {
+	console.log('PDFs: ' + tasks.length);
+	done();
+}
+
+var email = new Worker('email', emailHandler);
+var pdf = new Worker('pdf', 2, pdfHandler);
