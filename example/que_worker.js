5f7b15079b1127fbde28d6fb484dd34c15573b64
Evgeny Reznichenko <kusakyky@gmail.com>
2012-07-31 19:01:31 +0400

    Таски в очередь

--- example/que_worker.js
+++ example/que_worker.js
@@ -1,30 +1,29 @@
-var Que = require('../').Que,
-	Worker = require('../').Worker;
+var TaskQue = require('../lib/TaskQue.js');
 
-var que = new Que();
+var que1 = new TaskQue('app1');
 
 
 setTimeout(function () {
 	console.log('push to que');
-	
-	que.task('email', { address: 'da2@da.ru' });
-	que.task('email', { address: 'da3@da.ru' });
-	que.task('email', { address: 'da4@da.ru' });
-	que.task('email', { address: 'da5@da.ru' });
-	que.task('email', { address: 'da6@da.ru' });
-	que.task('email', { address: 'da7@da.ru' });
-	que.task('email', { address: 'da8@da.ru' });
-	que.task('email', { address: 'da9@da.ru' });
-	que.task('pdf', { address: 'pdf1' });
-	que.task('pdf', { address: 'pdf2' });
-	que.task('pdf', { address: 'pdf3' });
-	que.task('pdf', { address: 'pdf4' });
-	que.task('pdf', { address: 'pdf5' });
-	que.task('pdf', { address: 'pdf6' });
-	que.task('pdf', { address: 'pdf7' });
-	que.task('email', { address: 'da@da.ru' }, function (err, task) {
+	debugger;
+	que1.task('email', { address: 'da2@da.ru' });
+	que1.task('email', { address: 'da3@da.ru' });
+	que1.task('email', { address: 'da4@da.ru' });
+	que1.task('email', { address: 'da5@da.ru' });
+	que1.task('email', { address: 'da6@da.ru' });
+	que1.task('email', { address: 'da7@da.ru' });
+	que1.task('email', { address: 'da8@da.ru' });
+	que1.task('email', { address: 'da9@da.ru' });
+	que1.task('pdf', { address: 'pdf1' });
+	que1.task('pdf', { address: 'pdf2' });
+	que1.task('pdf', { address: 'pdf3' });
+	que1.task('pdf', { address: 'pdf4' });
+	que1.task('pdf', { address: 'pdf5' });
+	que1.task('pdf', { address: 'pdf6' });
+	que1.task('pdf', { address: 'pdf7' });
+	que1.task('email', { address: 'da@da.ru' }, function (err, task) {
 		console.log('Add task: %s, error: %s', task, err);
-		que.end();
+		que1.end();
 	});
 }, 3000);
 
@@ -39,5 +38,5 @@ function pdfHandler(tasks, done) {
 	done();
 }
 
-var email = new Worker('email', emailHandler);
-var pdf = new Worker('pdf', 2, pdfHandler);
+que1.process('email', emailHandler);
+que1.process('pdf', 2, pdfHandler);
