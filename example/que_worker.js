6008afe928350cd502d7098f62cd68bc93923ba9
Evgeny Reznichenko <kusakyky@gmail.com>
2012-08-01 13:59:22 +0400

    Сохранение заданий

--- example/que_worker.js
+++ example/que_worker.js
@@ -5,37 +5,47 @@ var que1 = new TaskQue('app1');
 
 setTimeout(function () {
 	console.log('push to que');
+	var t;
+	
 	debugger;
-	que1.task('email', { address: 'da2@da.ru' });
-	que1.task('email', { address: 'da3@da.ru' });
-	que1.task('email', { address: 'da4@da.ru' });
-	que1.task('email', { address: 'da5@da.ru' });
-	que1.task('email', { address: 'da6@da.ru' });
-	que1.task('email', { address: 'da7@da.ru' });
-	que1.task('email', { address: 'da8@da.ru' });
-	que1.task('email', { address: 'da9@da.ru' });
-	que1.task('pdf', { address: 'pdf1' });
-	que1.task('pdf', { address: 'pdf2' });
-	que1.task('pdf', { address: 'pdf3' });
-	que1.task('pdf', { address: 'pdf4' });
-	que1.task('pdf', { address: 'pdf5' });
-	que1.task('pdf', { address: 'pdf6' });
-	que1.task('pdf', { address: 'pdf7' });
-	que1.task('email', { address: 'da@da.ru' }, function (err, task) {
-		console.log('Add task: %s, error: %s', task, err);
-		que1.end();
+	que1.task('email', { address: 'da2@da.ru' }).save();
+	que1.task('email', { address: 'da3@da.ru' }).save();
+	que1.task('email', { address: 'da4@da.ru' }).save();
+	que1.task('email', { address: 'da5@da.ru' }).save();
+	que1.task('email', { address: 'da6@da.ru' }).save();
+	que1.task('email', { address: 'da7@da.ru' }).save();
+	que1.task('email', { address: 'da8@da.ru' }).save();
+	que1.task('email', { address: 'da9@da.ru' }).save();
+	que1.task('pdf', { address: 'pdf1' }).save();
+	que1.task('pdf', { address: 'pdf2' }).save();
+	que1.task('pdf', { address: 'pdf3' }).save();
+	que1.task('pdf', { address: 'pdf4' }).save();
+	que1.task('pdf', { address: 'pdf5' }).save();
+	que1.task('pdf', { address: 'pdf6' }).save();
+	que1.task('pdf', { address: 'pdf7' }).save();
+	t = que1.task('email', { address: 'da@da.ru' }).save(function (err) {
+		console.log('Add task: %s, error: %s', t, err);
+		que1.quit();
 	});
 }, 3000);
 
 
-function emailHandler(task, done) {
+function emailHandler(task) {
 	console.log('Email: ' + task.data.address);
-	done();
+	task.done();
 }
 
-function pdfHandler(tasks, done) {
-	console.log('PDFs: ' + tasks.length);
-	done();
+function pdfHandler(tasks) {
+	var i = 0, l = tasks.length;
+	
+	console.log('PDFs: ' + l);
+		
+	(function next(i) {
+		if (l > i) {
+			tasks[i].done();
+			next(i++);
+		}
+	}(i));
 }
 
 que1.process('email', emailHandler);
