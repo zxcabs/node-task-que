b20ef24fa007c04688cf3ed5dddbb391a2920512
Evgeny Reznichenko <kusakyky@gmail.com>
2012-08-02 16:08:10 +0400

    add app2

--- example/que_worker.js
+++ example/que_worker.js
@@ -1,10 +1,13 @@
 var TaskQue = require('../lib/TaskQue.js');
 
 var que1 = new TaskQue('app1');
+var que2 = new TaskQue('app2');
 
 
 setTimeout(function () {
 	console.log('push to que');
+	
+	//app2
 	que1.task('email', { address: 'da2@da.ru' }).save();
 	que1.task('email', { address: 'da3@da.ru' }).save();
 	que1.task('email', { address: 'da4@da.ru' }).save();
@@ -21,16 +24,20 @@ setTimeout(function () {
 	que1.task('pdf', { name: 'pdf6' }).save();
 	que1.task('pdf', { name: 'pdf7' }).save();
 	que1.task('email', { address: 'da@da.ru' }).save();
+	
+	//App2
+	que2.task('email', { address: 'APP2asdas23fsdf.tu' }).save();
+	que2.task('email', { address: 'APP22@adf.com' }).save();
 }, 3000);
 
 
 function emailHandler(task) {
-	console.log('get email: ' + task.data.address);
+	console.log('APP1: get email: ' + task.data.address);
 	
 	debugger;
 	setTimeout(function () {
 		task.done(function (err) {
-			console.log('done email: %s - error: %s', task.data.address, err);
+			console.log('APP1: done email: %s - error: %s', task.data.address, err);
 		});
 	}, 500);
 }
@@ -38,15 +45,15 @@ function emailHandler(task) {
 function pdfHandler(tasks) {
 	var l = tasks.length;
 	
-	console.log('PDFs: ' + l);
+	console.log('APP1: PDFs: ' + l);
 		
 	(function next(i) {
 		if (l > i) {
-			console.log('Get PDF: %s', tasks[i].data.name);
+			console.log('APP1: Get PDF: %s', tasks[i].data.name);
 			
 			setTimeout(function () {
 				tasks[i].done(function (err) {
-					console.log('Done PDF: %s', tasks[i].data.name);
+					console.log('APP1: Done PDF: %s', tasks[i].data.name);
 					next(++i);
 				});
 			}, 1000);
@@ -56,3 +63,16 @@ function pdfHandler(tasks) {
 
 que1.process('email', emailHandler);
 que1.process('pdf', { count: 2 }, pdfHandler);
+
+//app2
+que2.process('email', { count: 100 }, function (emails) {
+	console.log('APP1: get emails: %s', emails.length);
+	
+	(function next(email) {
+		if (email) {
+			console.log('APP2: get email: %s', email.data.address);
+			email.done();
+			next(emails.shift());
+		}
+	}(emails.shift()));
+});
