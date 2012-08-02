7a3ca019512dfe8aaa8ec30cde2090ee97985f8b
Evgeny Reznichenko <kusakyky@gmail.com>
2012-08-02 13:51:34 +0400

    Обработка тасков

--- example/que_worker.js
+++ example/que_worker.js
@@ -5,62 +5,54 @@ var que1 = new TaskQue('app1');
 
 setTimeout(function () {
 	console.log('push to que');
-	var t, t0;
-	
-	debugger;
-	t0 = que1.task('email', { address: 'da2@da.ru' }).save();
+	que1.task('email', { address: 'da2@da.ru' }).save();
 	que1.task('email', { address: 'da3@da.ru' }).save();
 	que1.task('email', { address: 'da4@da.ru' }).save();
-	que1.task('email', { address: 'da5@da.ru' }).save(function () {
-		que1.shift('email', function (err, task) {
-			//TODO task -> task.id
-			console.log('shift email que task: %s, error: %s', task, err);
-		});
-	});
+	que1.task('email', { address: 'da5@da.ru' }).save();
 	que1.task('email', { address: 'da6@da.ru' }).save();
 	que1.task('email', { address: 'da7@da.ru' }).save();
 	que1.task('email', { address: 'da8@da.ru' }).save();
 	que1.task('email', { address: 'da9@da.ru' }).save();
-	que1.task('pdf', { address: 'pdf1' }).save();
-	que1.task('pdf', { address: 'pdf2' }).save();
-	que1.task('pdf', { address: 'pdf3' }).save();
-	que1.task('pdf', { address: 'pdf4' }).save();
-	que1.task('pdf', { address: 'pdf5' }).save();
-	que1.task('pdf', { address: 'pdf6' }).save();
-	que1.task('pdf', { address: 'pdf7' }).save(function () {
-		que1.shift('pdf', 2, function (err, task) {
-			//TODO task -> task.id
-			console.log('shift pdf que task: %s, error: %s', task, err);
-		});
-	});
-	t = que1.task('email', { address: 'da@da.ru' }).save(function (err) {
-		console.log('Add task: %s, error: %s', t.id, err);
-		t0.done('nice mail!');
-		t.remove(function (err) {
-			console.log('remove task: %s, error: %s', t.id, err);
-			que1.quit();
-		});
-	});
+	que1.task('pdf', { name: 'pdf1' }).save();
+	que1.task('pdf', { name: 'pdf2' }).save();
+	que1.task('pdf', { name: 'pdf3' }).save();
+	que1.task('pdf', { name: 'pdf4' }).save();
+	que1.task('pdf', { name: 'pdf5' }).save();
+	que1.task('pdf', { name: 'pdf6' }).save();
+	que1.task('pdf', { name: 'pdf7' }).save();
+	que1.task('email', { address: 'da@da.ru' }).save();
 }, 3000);
 
 
 function emailHandler(task) {
-	console.log('Email: ' + task.data.address);
-	task.done();
+	console.log('get email: ' + task.data.address);
+	
+	debugger;
+	setTimeout(function () {
+		task.done(function (err) {
+			console.log('done email: %s - error: %s', task.data.address, err);
+		});
+	}, 500);
 }
 
 function pdfHandler(tasks) {
-	var i = 0, l = tasks.length;
+	var l = tasks.length;
 	
 	console.log('PDFs: ' + l);
 		
 	(function next(i) {
 		if (l > i) {
-			tasks[i].done();
-			next(i++);
+			console.log('Get PDF: %s', tasks[i].data.name);
+			
+			setTimeout(function () {
+				tasks[i].done(function (err) {
+					console.log('Done PDF: %s', tasks[i].data.name);
+					next(++i);
+				});
+			}, 1000);
 		}
-	}(i));
+	}(0));
 }
 
 que1.process('email', emailHandler);
-que1.process('pdf', 2, pdfHandler);
+que1.process('pdf', { count: 2 }, pdfHandler);
